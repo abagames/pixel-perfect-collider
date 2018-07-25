67688867164d77b271e08af49f1e09681f04bb61
ABA Games <cs8k-cyu@asahi-net.or.jp>
2018-07-25 23:36:23 +0900

    Add text handler

---
+++ src/samples/text.ts
@@ -0,0 +1,33 @@
+import * as WebFont from "webfontloader";
+import { screen } from "./screen";
+
+export let family: string;
+
+export function init(
+  _family = "Hannari",
+  url = "https://fonts.googleapis.com/earlyaccess/hannari.css"
+) {
+  family = _family;
+  return new Promise((resolve, reject) => {
+    WebFont.load({
+      custom: {
+        families: [family],
+        urls: [url]
+      },
+      active: () => resolve(),
+      inactive: () => reject()
+    });
+  });
+}
+
+export function add(x, y, size = 14, fill = "white") {
+  const text = new PIXI.Text("", {
+    fontFamily: family,
+    fontSize: size,
+    fill
+  });
+  text.x = x;
+  text.y = y;
+  screen.addChild(text);
+  return text;
+}
