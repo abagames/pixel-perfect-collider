3de41b46532d65a7fe9fa541c2b2c05d9846c459
ABA Games <cs8k-cyu@asahi-net.or.jp>
2018-07-27 10:11:17 +0900

    Change import

--- src/samples/shift.ts
+++ src/samples/shift.ts
@@ -1,32 +1,32 @@
 import * as pag from "pixel-art-gen";
 import * as screen from "./screen";
-import * as actor from "./actor";
+import { Actor, update as updateActors } from "./actor";
 import * as particle from "./particle";
 
-let stage_: actor.Actor;
+let stage_: Actor;
 
 window.onload = () => {
   screen.init();
-  stage_ = new actor.Actor(stage);
+  stage_ = new Actor(stage);
   update();
 };
 
 function update() {
   requestAnimationFrame(update);
   particle.update();
-  actor.update();
+  updateActors();
 }
 
-function stage(a: actor.Actor) {
+function stage(a: Actor) {
   if (a.isSpawning) {
-    new actor.Actor(ship);
+    new Actor(ship);
   }
   if (Math.random() < 0.1) {
     particle.emit("e1", Math.random() * 256, Math.random() * 256);
   }
 }
 
-async function ship(a: actor.Actor) {
+async function ship(a: Actor) {
   if (a.isSpawning) {
     const images = await pag.generateImagesPromise(
       `
